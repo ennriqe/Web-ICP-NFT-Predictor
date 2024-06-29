aac6b1bc8db4fb82a60d4fad1cd2451b80e12965
Enrique <ennriqe@Enriques-MacBook-Pro.local>
2024-06-29 13:18:16 +0200

    trying to change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,36 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/main.js', // Entry point for your main.js file
-  output: {
-    filename: 'bundle.js', // Output bundle file
-    path: path.resolve(__dirname, 'dist'), // Output directory
-  },
-  resolve: {
-    extensions: ['.js', '.json'], // Resolve these extensions
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader', // Use Babel to transpile JavaScript
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.js', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html', // Use the index.html file as template
-    }),
-  ],
+    // Exclude node_modules from the bundle
+    externals: [NodeExternals()],
 };
